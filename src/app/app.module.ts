d253dbdd515218d0eacb43053a0cc64389a31541
Felipe <felipearizac@gmail.com>
2018-08-04 18:24:34 -0500

    Agregando algunos estilos, imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-/*
+
 import { IndexComponent } from './component/index';
 import { SlideComponent } from './component/slide';
 import { ContainerComponent} from './component/container';
-import { FooterComponent } from './component/footer';*/
+import { FooterComponent } from './component/footer';
  
 @NgModule({
   declarations: [
     AppComponent,
-   /* IndexComponent, 
+    IndexComponent, 
     SlideComponent, 
     ContainerComponent, 
-    FooterComponent*/
+    FooterComponent
   ],
   imports: [
     BrowserModule
