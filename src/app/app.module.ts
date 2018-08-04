bb9da5d24602a41fb3353e6db516adc62bd295f0
Felipe <felipearizac@gmail.com>
2018-08-04 18:17:10 -0500

    Iniciando proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+/*
+import { IndexComponent } from './component/index';
+import { SlideComponent } from './component/slide';
+import { ContainerComponent} from './component/container';
+import { FooterComponent } from './component/footer';*/
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   /* IndexComponent, 
+    SlideComponent, 
+    ContainerComponent, 
+    FooterComponent*/
   ],
   imports: [
     BrowserModule
